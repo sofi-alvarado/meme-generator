61ee7306cae88501857e098302fcf6d740408092
Sofía Alvarado <alvaradosofiaH@outlook.com>
2022-10-20 21:08:10 -0600

    Changes to the Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import navLogo from '../images/troll-face.png'
+import headerImg from '../images/troll-face.png'
 
 export default function Header(){
     return (
-        <nav>
-            <img src={navLogo} className="nav--icon"/>
-            <h3 className="nav--logo_text white">Meme Generator</h3>
-            <h4 className="nav--title white">React Course - Project 3</h4> 
-        </nav>
+        <header className='header'>
+            <img src={headerImg} className="header--image"/>
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4> 
+        </header>
     )
 }
